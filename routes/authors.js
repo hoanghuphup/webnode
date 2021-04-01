f024f00805ffaf1f318d01da6f90bc60693aade9
hoanghuphup <73218868+hoanghuphup@users.noreply.github.com>
2021-04-01 20:58:15 +0700

    create,new routes

--- routes/authors.js
+++ routes/authors.js
@@ -3,15 +3,15 @@ const router = express.Router()
 const Author= require('../models/author')
 
 router.get('/', async (req,res)=>{
-    let search={}
+    let searchOptions={}
     if(req.query.name!=null&&req.query.name!==''){
-        search.name=new RegExp(req.query.name,'i')
+        searchOptions.name=new RegExp(req.query.name,'i')
     }
     try{
-        const authors=await Author.find(search)
+        const authors=await Author.find(searchOptions)
         res.render('authors/index',{
             authors:authors,
-            search:req.query
+            searchOptions:req.query
         })
     }catch{
         res.redirect('/')
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express=require('express')
 const app = express()
 const expressLayouts= require('express-ejs-layouts')
 const bodyParser=require('body-parser')
+  
 
 const mongoose= require('mongoose')
 mongoose.connect(process.env.DATABASE_URL,{
@@ -19,6 +20,8 @@ db.once('open',error =>console.log('connected to mongoose'))
 
 const indexRouter=require('./routes/index')
 const authorRouter=require('./routes/authors')
+const bookRouter = require('./routes/books')
+
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
@@ -29,5 +32,6 @@ app.use(bodyParser.urlencoded({limit:'10mb',extended: false}))
 
 app.use('/',indexRouter)
 app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
 
 app.listen(process.env.PORT || 3000)
